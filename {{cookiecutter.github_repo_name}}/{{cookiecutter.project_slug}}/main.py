3dc9cbb6c0841bc6cbd45624796fd75b4bd04b3d
Omar <omar@atumm.org>
2023-02-21 18:32:16 +0200

    migrate to PDM

---
+++ {{cookiecutter.github_repo_name}}/{{cookiecutter.project_slug}}/main.py
@@ -0,0 +1,12 @@
+import sys
+from PySide6.QtWidgets import (QApplication)
+
+from appening.appening import MainWindow
+
+app = QApplication(sys.argv)
+
+main_window = MainWindow('{{ cookiecutter.webapp_url }}')
+available_geometry = main_window.screen().availableGeometry()
+main_window.resize(available_geometry.width(), available_geometry.height())
+main_window.show()
+sys.exit(app.exec())
